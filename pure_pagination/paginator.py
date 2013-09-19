33f7a55e6949964032b9bd2fabe6846bb1e6f32b
Andrey Balandin <andrvb@gmail.com>
2013-09-19 18:15:06 +0400

    no xrange in python 3

--- pure_pagination/paginator.py
+++ pure_pagination/paginator.py
@@ -83,7 +83,7 @@ class Page(BasePage):
         elif self.number < settings.PAGE_RANGE_DISPLAYED / 2:
             left_side = self.number
             right_side = settings.PAGE_RANGE_DISPLAYED - left_side
-        for page in xrange(1, self.paginator.num_pages + 1):
+        for page in range(1, self.paginator.num_pages + 1):
             if page <= settings.MARGIN_PAGES_DISPLAYED:
                 result.append(page)
                 continue
